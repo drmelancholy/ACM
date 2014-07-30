4630d77c6254024dac06cb21b81e133bb1018db8
drmelancholy <linuxosman@gmail.com>
2014-07-30 12:42:29 -0700

    repository

---
+++ ACM.BLTest/OrderRepositoryTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class OrderRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveExisting()
+        {
+            var orderRepository = new OrderRepository();
+            var expected = new Order(10)
+            {
+                OrderDate = Convert.ToDateTime("2014-07-21 14:10:10")
+            };
+
+            var actual = orderRepository.Retrieve(10);
+
+            //Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected.OrderID, actual.OrderID);
+            Assert.AreEqual(expected.OrderDate, actual.OrderDate);
+            
+        }
+    }
+}
---
+++ ACM.BLTest/ProductRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class ProductRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveExisting()
+        {
+            var productRepository = new ProductRepository();
+            var expected = new Product(2)
+            {
+                ProductName = "SunFlowers",
+                ProductDescription = "Assorted Size",
+                CurrentPrice = 15.96M
+            };
+
+            var actual = productRepository.Retrieve(2);
+
+            //Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected.ProductId, actual.ProductId);
+            Assert.AreEqual(expected.ProductName, actual.ProductName);
+            Assert.AreEqual(expected.ProductDescription, actual.ProductDescription);
+            Assert.AreEqual(expected.CurrentPrice, actual.CurrentPrice);
+        }
+    }
+}
